c04ab910a742e6faa082641d538b9e2ca3fc503c
Ziyana-prodigy <zsapz01@gmail.com>
2024-11-02 17:20:00 +0530

    Add files via upload

---
+++ useless/load.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    // Hide the loading screen and show the content
+    document.getElementById('loadingScreen').classList.add('hidden');
+    document.getElementById('content').style.display = 'block';
+});
