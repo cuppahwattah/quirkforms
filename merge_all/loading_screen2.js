9a959233dd57fee20d5bb696c0458bb8fbb3a660
cuppahwattah <fathimaridaps@gmail.com>
2024-11-03 12:29:02 +0530

    Delete merge_all/loading_screen2.js

--- merge_all/loading_screen2.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        document.getElementById("preloader").style.display = "none"; 
-        document.getElementById("content").style.display = "block"; 
-    }, 3000); 
-});
