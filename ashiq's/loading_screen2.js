02b65893e41709d52d1a38b35d6151067b32fd6e
cuppahwattah <fathimaridaps@gmail.com>
2024-11-03 11:23:29 +0530

    Delete ashiq's directory

--- ashiq's/loading_screen2.js
+++
@@ -1,9 +0,0 @@
-// script.js
-
-document.addEventListener("DOMContentLoaded", function() {
-    // Simulate loading time
-    setTimeout(function() {
-        document.getElementById("preloader").style.display = "none"; // Hide preloader
-        document.getElementById("content").style.display = "block"; // Show content
-    }, 3000); // Adjust time as needed
-});
