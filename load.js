9ebd4eb3ade3d8928283a35787dc2657df0711d5
cuppahwattah <fathimaridaps@gmail.com>
2024-11-03 11:15:26 +0530

    Delete load.js

--- load.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function() {
-    // Hide the loading screen and show the content
-    document.getElementById('loadingScreen').classList.add('hidden');
-    document.getElementById('content').style.display = 'block';
-});
